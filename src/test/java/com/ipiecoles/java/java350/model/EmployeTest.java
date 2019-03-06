e04acc766b3784861d440b0591208c162fcd64c9
Zonthem <nico.schtroumpfe@gmail.com>
2019-03-06 12:11:10 +0100

    delete sonar de merde et création fichier test

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,13 @@
+package com.ipiecoles.java.java350.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeTest {
+
+    @Test
+    void getNombreAnneeAnciennete() {
+
+    }
+}
