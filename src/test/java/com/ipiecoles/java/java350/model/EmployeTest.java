3c5c6b3cef759a897cb60f06acea33b29b4459bf
Zonthem <nico.schtroumpfe@gmail.com>
2019-03-06 15:15:40 +0100

    test Employe getPrimeannuelle

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java350.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 
@@ -51,5 +53,22 @@ class EmployeTest {
         Integer nbAnneeAnciennete= e.getNombreAnneeAnciennete();
         //Then
         Assertions.assertEquals(0, (int)nbAnneeAnciennete);
+    }
+
+    @ParameterizedTest(name = "Employe anciennete : {0}, matricule : {1}, performance : {2}, temps partiel : {3} a une prime de {4}")
+    @CsvSource({
+            "0, 'M02586', 1, 1D, 1700D",
+            "0, 'T02586', 1, 1D, 1000D",
+            "0, 'M02586', 1, 0.5D, 850D",
+            "1, 'M02586', 1, 1D, 1800D",
+            "0, 'M02586', 1, 0D, 0D",
+    })
+    void testGetPrimeAnnuelle(int dtEmbauche, String mat, int perf, double tp, double expected) {
+        //Given
+        Employe e = new Employe("nom", "prenom", mat, LocalDate.now().minusYears(dtEmbauche), 2000D, perf, tp);
+        //When
+        double result = e.getPrimeAnnuelle();
+        //Then
+        Assertions.assertEquals(expected, result);
     }
 }
