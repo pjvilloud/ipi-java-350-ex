b82cf712240b800ac27864d8aa6c44dbdb7405fd
Raydone <b.milliet@me.com>
2019-03-06 12:12:02 +0100

    add unit test and remove sonar script

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,17 @@
+package com.ipiecoles.java.java350.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeTest {
+
+    @Test
+    void getNombreAnneeAnciennete() {
+        //given
+        
+        //when
+
+        //then
+    }
+}
