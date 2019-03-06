ea176eca55069906dbf4367800b8a36066839ce8
Raydone <b.milliet@me.com>
2019-03-06 15:44:19 +0100

    others tests

---
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.ipiecoles.java.java350.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class EmployeRepositoryTest {
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    @Test
+    public void testFindLastMatriculeEmpty(){
+        //Given
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //Then
+        Assertions.assertNull(lastMatricule);
+    }
+
+}
