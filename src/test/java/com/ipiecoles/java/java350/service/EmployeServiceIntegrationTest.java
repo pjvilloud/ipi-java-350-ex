a87015da37860ceb0ace3073ad03a363b9896f1a
Raydone <b.milliet@me.com>
2019-03-07 09:53:20 +0100

    create new file

---
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.ipiecoles.java.java350.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeServiceIntegrationTest {
+
+    @Test
+    void embaucheEmployeTest() {
+        
+    }
+}
