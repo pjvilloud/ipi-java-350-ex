5d76f0634ea1e58c8a8801f8a4a1b4d3299eba81
xaviercollion <xavier.collion@cgi.com>
2019-04-11 22:53:14 +0200

    plus de code smells

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -37,8 +37,7 @@ public class EmployeService {
      * @throws EntityExistsException Si le matricule correspond à un employé existant
      */
     public void embaucheEmploye(String nom, String prenom, Poste poste, NiveauEtude niveauEtude, Double tempsPartiel) throws EmployeException, EntityExistsException {
-        logger.debug("Coucou");
-        logger.info("Embauche de l'employé {} {} diplômé de {} en tant que {} avec un taux d'activité de {} ", prenom, nom, niveauEtude, poste, tempsPartiel);
+        
 
         //Récupération du type d'employé à partir du poste
         String typeEmploye = poste.name().substring(0,1);
@@ -51,7 +50,7 @@ public class EmployeService {
         //... et incrémentation
         Integer numeroMatricule = Integer.parseInt(lastMatricule) + 1;
         if(numeroMatricule >= 100000){
-            logger.error("Limite des 100000 matricules atteinte !");
+
             throw new EmployeException("Limite des 100000 matricules atteinte !");
         }
         //On complète le numéro avec des 0 à gauche
@@ -60,7 +59,7 @@ public class EmployeService {
 
         //On vérifie l'existence d'un employé avec ce matricule
         if(employeRepository.findByMatricule(matricule) != null){
-            logger.error("L'employé de matricule {} existe déjà en BDD", matricule);
+
             throw new EntityExistsException("L'employé de matricule " + matricule + " existe déjà en BDD");
         }
 
@@ -128,9 +127,9 @@ public class EmployeService {
             performance = employe.getPerformance() + 1;
         }
         //Cas 5
-
+        else if(caTraite > objectifCa*1.2){
             performance = employe.getPerformance() + 4;
-        
+        }
         //Si autre cas, on reste à la performance de base.
 
         //Calcul de la performance moyenne
