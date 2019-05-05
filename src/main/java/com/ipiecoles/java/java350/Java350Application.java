b6b0705d7f741de8c4b09a1e75176db681f9355e
Jordan <jordanbouget@gmail.com>
2019-05-05 16:31:09 +0200

    correct code smells

--- src/main/java/com/ipiecoles/java/java350/Java350Application.java
+++ src/main/java/com/ipiecoles/java/java350/Java350Application.java
@@ -1,16 +1,12 @@
 package com.ipiecoles.java.java350;
 
-import com.ipiecoles.java.java350.model.Employe;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class Java350Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Java350Application.class, args);
     }
 
 }
