62956e2642695795929d360113524135a48715c7
kabalord <wroaserrano@gmail.com>
2019-05-24 02:11:57 +0200

    Correction troisième partie sonar cloud

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -34,7 +34,7 @@ public class EmployeService {
      * @throws EmployeException Si on arrive au bout des matricules possibles
      * @throws EntityExistsException Si le matricule correspond à un employé existant
      */
-    public Employe embaucheEmploye(String nom, String prenom, Poste poste, NiveauEtude niveauEtude, Double tempsPartiel) throws EmployeException, EntityExistsException {
+    public Employe embaucheEmploye(String nom, String prenom, Poste poste, NiveauEtude niveauEtude, Double tempsPartiel) throws EmployeException {
 
         //Log des paramètres de la méthode : INFO
         LOG.info("Embauche d'un employé {} {} {} {} {}", nom, prenom, poste, niveauEtude, tempsPartiel);
@@ -98,38 +98,41 @@ public class EmployeService {
      * @throws EmployeException Si le matricule est null ou ne commence pas par un C
      */
     public void calculPerformanceCommercial(String matricule, Long caTraite, Long objectifCa) throws EmployeException {
+
         //Vérification des paramètres d'entrée
+
         if(caTraite == null || caTraite < 0){
             throw new EmployeException("Le chiffre d'affaire traité ne peut être négatif ou null !");
-        }
-        if(objectifCa == null || objectifCa < 0){
+        }else if(objectifCa == null || objectifCa < 0){
             throw new EmployeException("L'objectif de chiffre d'affaire ne peut être négatif ou null !");
-        }
-        if(matricule == null || !matricule.startsWith("C")){
+        }else if(matricule == null || !matricule.startsWith("C")){
             throw new EmployeException("Le matricule ne peut être null et doit commencer par un C !");
         }
+
         //Recherche de l'employé dans la base
         Employe employe = employeRepository.findByMatricule(matricule);
+
         if(employe == null){
             throw new EmployeException("Le matricule " + matricule + " n'existe pas !");
         }
 
         Integer performance = Entreprise.PERFORMANCE_BASE;
         //Cas 2
-        if(caTraite >= objectifCa*0.8 && caTraite < objectifCa*0.95){
-            performance = Math.max(Entreprise.PERFORMANCE_BASE, employe.getPerformance() - 2);
+        if(caTraite > objectifCa*1.2){
+            performance = employe.getPerformance() + 4;
         }
         //Cas 3
-        else if(caTraite >= objectifCa*0.95 && caTraite <= objectifCa*1.05){
-            performance = Math.max(Entreprise.PERFORMANCE_BASE, employe.getPerformance());
-        }
-        //Cas 4
-        else if(caTraite <= objectifCa*1.2 && caTraite > objectifCa*1.05){
+        else if(caTraite > objectifCa*1.05){
             performance = employe.getPerformance() + 1;
         }
+        //Cas 4
+        else if(caTraite >= objectifCa*0.95){
+            performance = Math.max(Entreprise.PERFORMANCE_BASE, employe.getPerformance());
+        }
         //Cas 5
-        else if(caTraite > objectifCa*1.2){
-            performance = employe.getPerformance() + 4;
+        else if(caTraite >= objectifCa*0.8){
+            performance = Math.max(Entreprise.PERFORMANCE_BASE, employe.getPerformance() - 2);
+
         }
         //Si autre cas, on reste à la performance de base.
 
