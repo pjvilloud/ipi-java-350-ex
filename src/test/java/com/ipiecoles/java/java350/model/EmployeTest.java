5a8ed028085e6e5dec54a3f40c63df83e7be1a93
Florian Kerouani <florian.kerouani@ipilyon.net>
2019-08-30 10:21:39 +0200

    Test unitaire Employe.augmenterSalaire

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -7,58 +7,131 @@ import java.time.LocalDate;
 
 public class EmployeTest {
 
-    @Test
-    public void testGetNombreAnneeAncienneteNow(){
-        //Given
-        Employe employe = new Employe();
-        LocalDate dateEmbauche = LocalDate.now();
-        employe.setDateEmbauche(dateEmbauche);
+	@Test
+	public void testAugmenterSalaire0() {
+		// Given
+		Employe employe = new Employe();
+		employe.setSalaire(100d);
+		employe.augmenterSalaire(0);
 
-        //When
-        Integer nbAnnee = employe.getNombreAnneeAnciennete();
+		// When
+		Double nouveauSalaire = employe.getSalaire();
 
-        //Then
-        Assertions.assertThat(nbAnnee).isEqualTo(0);
-    }
+		// Then
+		Assertions.assertThat(nouveauSalaire).isEqualTo(100);
+	}
 
-    @Test
-    public void testGetNombreAnneeAncienneteNull(){
-        //Given
-        Employe e = new Employe();
-        e.setDateEmbauche(null);
+	@Test
+	public void testAugmenterSalaire50() {
+		// Given
+		Employe employe = new Employe();
+		employe.setSalaire(100d);
+		employe.augmenterSalaire(50);
 
-        //When
-        Integer nbAnnee = e.getNombreAnneeAnciennete();
+		// When
+		Double nouveauSalaire = employe.getSalaire();
 
-        //Then
-        Assertions.assertThat(nbAnnee).isEqualTo(0);
-    }
+		// Then
+		Assertions.assertThat(nouveauSalaire).isEqualTo(150);
+	}
 
-    @Test
-    public void testGetNombreAnneeAncienneteNmoins2(){
-        //Given
-        Employe e = new Employe();
-        e.setDateEmbauche(LocalDate.now().minusYears(2));
+	@Test
+	public void testAugmenterSalaire200() {
+		// Given
+		Employe employe = new Employe();
+		employe.setSalaire(100d);
+		employe.augmenterSalaire(200);
 
-        //When
-        Integer nbAnnee = e.getNombreAnneeAnciennete();
+		// When
+		Double nouveauSalaire = employe.getSalaire();
 
-        //Then
-        Assertions.assertThat(nbAnnee).isEqualTo(2);
-    }
+		// Then
+		Assertions.assertThat(nouveauSalaire).isEqualTo(300);
+	}
 
-    @Test
-    public void testGetNombreAnneeAncienneteNplus2(){
-        //Given
-        Employe e = new Employe();
-        e.setDateEmbauche(LocalDate.now().plusYears(2));
+	@Test
+	public void testAugmenterSalaireNegatif50() {
+		// Given
+		Employe employe = new Employe();
+		employe.setSalaire(100d);
+		employe.augmenterSalaire(-50);
 
-        //When
-        Integer nbAnnee = e.getNombreAnneeAnciennete();
+		// When
+		Double nouveauSalaire = employe.getSalaire();
 
-        //Then
-        Assertions.assertThat(nbAnnee).isEqualTo(0);
-    }
+		// Then
+		Assertions.assertThat(nouveauSalaire).isEqualTo(50);
+	}
 
+	@Test
+	public void testAugmenterSalaireNegatif101() {
+		// Given
+		Employe employe = new Employe();
+		employe.setSalaire(100d);
+		IllegalArgumentException exception = null;
+
+		// When
+		try {
+			employe.augmenterSalaire(-101);
+		} catch (IllegalArgumentException e) {
+			exception = e;
+		}
+
+		// Then
+		Assertions.assertThat(exception).isNotNull();
+	}
+
+	@Test
+	public void testGetNombreAnneeAncienneteNow() {
+		// Given
+		Employe employe = new Employe();
+		LocalDate dateEmbauche = LocalDate.now();
+		employe.setDateEmbauche(dateEmbauche);
+
+		// When
+		Integer nbAnnee = employe.getNombreAnneeAnciennete();
+
+		// Then
+		Assertions.assertThat(nbAnnee).isEqualTo(0);
+	}
+
+	@Test
+	public void testGetNombreAnneeAncienneteNull() {
+		// Given
+		Employe e = new Employe();
+		e.setDateEmbauche(null);
+
+		// When
+		Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+		// Then
+		Assertions.assertThat(nbAnnee).isEqualTo(0);
+	}
+
+	@Test
+	public void testGetNombreAnneeAncienneteNmoins2() {
+		// Given
+		Employe e = new Employe();
+		e.setDateEmbauche(LocalDate.now().minusYears(2));
+
+		// When
+		Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+		// Then
+		Assertions.assertThat(nbAnnee).isEqualTo(2);
+	}
+
+	@Test
+	public void testGetNombreAnneeAncienneteNplus2() {
+		// Given
+		Employe e = new Employe();
+		e.setDateEmbauche(LocalDate.now().plusYears(2));
+
+		// When
+		Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+		// Then
+		Assertions.assertThat(nbAnnee).isEqualTo(0);
+	}
 
 }
