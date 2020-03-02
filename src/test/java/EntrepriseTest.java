710edffa8d65c16d1c583e5328a4d2bf5c4062e2
aloys.theodore <aloys.theodore@cgi.com>
2020-03-02 15:00:50 +0100

    add test unit

---
+++ src/test/java/EntrepriseTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntrepriseTest {
+
+    @Test
+    void primeAnnuelleBase() {
+    }
+
+    @Test
+    void joursFeries() {
+    }
+}
---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,48 @@
+package com.ipiecoles.java.java350.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class EmployeTest {
+
+    @Test
+    public void testGetNombreAnneeAncienneteDtEmbaucheNull() {
+        //Given Création employe avec une date embauche à null
+        Employe employe = new Employe();
+        employe.setDateEmbauche(null);
+
+        // When Initiatiolisation d'un nb d'année d'anciennete
+        Integer nbAnneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then Vérification que c'est bien à 0
+        Assertions.assertThat(nbAnneeAnciennete).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteDtEmbaucheTodayPlus2() {
+        //Given Création employe avec une date embauche à null
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().plusYears(2));
+
+        // When Initiatiolisation d'un nb d'année d'anciennete
+        Integer nbAnneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then Vérification que c'est bien à 0
+        Assertions.assertThat(nbAnneeAnciennete).isEqualTo(0);
+    }
+
+    @Test
+    public void testGetNombreAnneeAncienneteDtEmbaucheTodayMinus3() {
+        //Given Création employe avec une date embauche à null
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(3));
+
+        // When Initiatiolisation d'un nb d'année d'anciennete
+        Integer nbAnneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then Vérification que c'est bien à 0
+        Assertions.assertThat(nbAnneeAnciennete).isEqualTo(3);
+    }
+}
