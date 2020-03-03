5b77167a7acdfe8787c46e4a856ecf2b6d4bf8b3
aloys.theodore <aloys.theodore@cgi.com>
2020-03-03 14:40:02 +0100

    update pom to execute tests

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
@@ -1,4 +1,29 @@
 package com.ipiecoles.java.java350.service;
 
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class EmployeServiceIntegrationTest {
+
+    @Autowired
+    EmployeService employeService;
+
+    @Autowired
+    EmployeRepository employeRepository;
+
+    @BeforeEach
+    public void setup(){
+        employeRepository.deleteAll();
+    }
+
+    public void testIntegrationEmploye(){
+        //Given
+
+        //When
+
+        //Then
+    }
 }
