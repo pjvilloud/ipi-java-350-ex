c9113bcafb73c7917bd31d2b18121801008b0d51
IBRAHIMI <nasim.ibrahimi@cgi.com>
2020-03-09 16:45:21 +0100

    adding repositiry test

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -17,12 +17,25 @@ public class EmployeRepositoryTest {
 
 	    @Autowired
 	    EmployeRepository employeRepository;
+	    
+		@Test
+		public void testFindByImmatNull(){
+		    //Given
+		    
+		    //When
+		    String result = employeRepository.findLastMatricule();
+		    
+		    //Then
+		    Assertions.assertThat(result).isNull(); 
+	}
+		
+		
 	    @Test
 	    public void testFindByImmat(){
 	        //Given
 	    	Employe emp3 = employeRepository.save(new Employe("emp3", "emp3", "X55555", LocalDate.now().minusMonths(10), 2500.00, 3, 0.50));
-	        Employe emp = employeRepository.save(new Employe("emp1", "emp1", "X99999", LocalDate.now(), 2500.00, 3, 1.00));
-	        Employe emp2 = employeRepository.save(new Employe("emp2", "emp2", "X33333", LocalDate.now().minusMonths(8), 2500.00, 3, 1.00));
+	        Employe emp = employeRepository.save(new Employe("emp1", "emp1", "T99999", LocalDate.now(), 2500.00, 3, 1.00));
+	        Employe emp2 = employeRepository.save(new Employe("emp2", "emp2", "A33333", LocalDate.now().minusMonths(8), 2500.00, 3, 1.00));
 	        
 	        //When
 	        String result = employeRepository.findLastMatricule();
@@ -30,6 +43,7 @@ public class EmployeRepositoryTest {
 	        //Then
 	        Assertions.assertThat(result).isEqualTo("99999"); 
 	    }
-	}	
+
 	
+}
 
