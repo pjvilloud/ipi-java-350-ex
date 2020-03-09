e68da2a0db46d3a200d6c570c31013c62d377d8a
Séverine Rovera <severine.rovera@cgi.com>
2020-03-09 16:45:49 +0100

    Test sur repository

---
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.ipiecoles.java.java350.repository;
+
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ipiecoles.java.java350.model.Employe;
+
+@SpringBootTest
+public class EmployeRepositoryTest {
+	
+	@Autowired
+	EmployeRepository employeRepository;
+	
+	@Test
+	public void testFindLastMatricule0Emp() {
+		
+		//Given
+		
+		//When
+		String lastMatricule = employeRepository.findLastMatricule();
+		
+		//Then
+		Assertions.assertThat(lastMatricule).isNull();
+	}
+	
+	@Test
+	public void testFindLastMatricule2Emp() {
+		
+		//Given
+		Employe e1 = new Employe("Doe", "Jane", "M99999", LocalDate.now(), 1500d, 1, 1.0);
+		employeRepository.save(e1);
+		
+		Employe e2 = new Employe("Doe", "John", "T99998", LocalDate.now(), 1500d, 1, 1.0);
+		employeRepository.save(e2);
+		
+		//When
+		String lastMatricule = employeRepository.findLastMatricule();
+		
+		//Then
+		Assertions.assertThat(lastMatricule).isEqualTo("M99999"); 
+	}
+	
+
+}
