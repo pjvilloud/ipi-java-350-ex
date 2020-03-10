f7f85ab8ded9aae8ba3fc03d7f1653b6d907e0ff
PierreLouisMichel <michelpierrelouis48@gmail.com>
2020-03-10 12:50:40 +0100

    Commit_10-03

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -1,5 +1,32 @@
 package com.ipiecoles.java.java350.repository;
 
-public class EmployeRepositoryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.ipiecoles.java.java350.model.Employe;
+
+public class EmployeRepositoryTest 
+{
+	@RunWith(SpringRunner.class) //Junit 4
+	@ExtendWith(SpringExtension.class) //Junit 5 
+	@DataJpaTest // ou @SpringBootTest
+	public class VehiculeRepositoryTest
+	{
+
+	    @Autowired
+	    EmployeRepository employeRepository;
+	    @Test
+	    public void testFindByImmat(){
+	        //Given
+	        //Employe v = employeRepository.save(new Employe("AA-123-BB"));
+	        //When
+	        String lastMatricule = employeRepository.findLastMatricule();
+	        //Then
+	        Assertions.assertThat(lastMatricule).isNull(); 
+	    }
+	}
 }
--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -1,5 +1,92 @@
 package com.ipiecoles.java.java350.service;
 
-public class EmployeServiceTest {
+import java.time.LocalDate;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import com.ipiecoles.java.java350.exception.EmployeException;
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
+import com.ipiecoles.java.java350.model.NiveauEtude;
+import com.ipiecoles.java.java350.model.Poste;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeServiceTest 
+{
+	@InjectMocks
+	private EmployeService employeService;
+	
+	@Mock
+	private EmployeRepository employeRepository
+	
+	@Test
+	public void testEmbaucheEmployeCommercialPleinTempsBTS()
+	{
+		//Given
+		String nom;
+		String prenom;
+		Poste poste = Poste.COMMERCIAL;
+		NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+		Double tempsPartiel = 1.0;
+		//findLastMatricule => 00345 / null
+		Mockito.when(employeRepository.findLastMatricule()).thenReturn("00345");
+		//findByMatricule => null
+		Mockito.when(employeRepository.findByMatricule("C00346")).thenReturn(null);
+		
+		//When
+		employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+		
+		//Then
+		//BDD si l'employé est bien créer (nom, prenom, matricule, salaire, date d'embauche, performance).
+		ArgumentCaptor<Employe> employeArgumentCaptor = ArgumentCaptor<T>.forClass(Employe.class);
+		Mockito.verify(employeRepository, Mockito.times(wantedNumberOfInvocations:1)).save(employeArgumentCaptor.getValue();
+		Employe employe = employeArgumentCaptor.getValue();
+		
+		//Employe employeVerif = new Employe(nom,prenom, ...)
+		//
+		Assertions.assertThat(employe.getNom())isEqualTo(nom);
+		Assertions.assertThat(employe.getPrenom())isEqualTo(prenom);
+		Assertions.assertThat(employe.getNom())isEqualTo("C00346");
+		Assertions.assertThat(employe.getNom())isEqualTo(LocalDate.now());
+		Assertions.assertThat(employeArgumentCaptor.getValue().getDateEmbauche().format(DateTimeFormatter.ofPattern("yyMMdd")));
+		Assertions.assertThat(employe.getTempsPartiel())isEqualTo(Entreprise.PERFORMANCE_BASE);
+		Assertions.assertThat(employe.getPerformance())isEqualTo(1);
+		//1521.22*1.2*1.0
+		Assertions.assertThat(employe.getSalaire()).isEqualTo(1825.46);
+	}
+	
+	@Test
+	public void testEmbaucheEmployeLimiteMatricule() throws EmployeException()
+	{
+		//Given
+		String nom;
+		String prenom;
+		Poste poste = Poste.COMMERCIAL;
+		NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+		Double tempsPartiel = 1.0;
+		
+		Mockito.when(employeRepository.findLastMatricule()).thenReturn("99999");
+		
+		//When
+		try
+		{
+			employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+			Assertion.fail("Aurait du planter !");
+		}
+		
+		catch (Exception e)
+		{
+			//Then
+			Assertions.assertThat(e).isInstanceOf(EmployeException.class);
+			Assertions.assertThat(e.getMessage()).isEqualTo("Limite des 10000 matricules atteinte !");
+		}
+		
+	}
+	
 }
