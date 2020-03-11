b12a521318025df89d251ce57367f847ad2d99a5
Sayane <nuelsayane@gmail.com>
2020-03-11 19:59:40 +0100

    new maj

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
 import com.ipiecoles.java.java350.repository.EmployeRepository;
 
-@SpringBootTest
+//@SpringBootTest
+@DataJpaTest
 
 public class EmployeRepositoryTest {
 	
@@ -29,8 +32,8 @@ public class EmployeRepositoryTest {
 	public void testFindLastMatricule() {
 	
 	//Given
-	Employe employe1 = employeRepository.save(new Employe("Jack", "Jacjie", "M12345", LocalDate.now(), 1500d, 1, 1.0));
-	Employe employe2 = employeRepository.save(new Employe("Jack", "Jackie", "T02345", LocalDate.now(), 1500d, 1, 1.0));	
+	Employe employe1 = employeRepository.save(new Employe("Jack", "Jacjie", "M12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+	Employe employe2 = employeRepository.save(new Employe("Jack", "Jackie", "T02345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));	
 	
 	//When
 	String lastMatricule = employeRepository.findLastMatricule();
