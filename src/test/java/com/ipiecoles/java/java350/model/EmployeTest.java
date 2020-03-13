4b547e666aeeb2fdedd2ece8dd64bf1f51afc820
julienudev <julien.udev@gmail.com>
2020-03-13 09:27:35 +0100

    clean v2

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -1,50 +1,16 @@
 package com.ipiecoles.java.java350.model;
 
-
+import com.ipiecoles.java.java350.exception.EmployeException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import java.time.LocalDate;
 
 public class EmployeTest {
 
-    @Test
-    @DisplayName("Test AugmenterSalaire avec pourcentage négatif => salaire identique")
-    public void testAugmenterSalaire() throws Exception {
-
-        //Given
-        Employe employe = new Employe();
-        employe.setSalaire(1000.0);
-
-        //When
-        //Integer nbAnnees = employe.getNombreAnneeAnciennete();
-        Double nouveauSalaire = employe.augmenterSalaire( -0.5 );
-        //Double pourcentage=-0.5;
-        //Then
-        //Assertions.assertThat(nbAnnees).isEqualTo(2);
-        Assertions.assertThat(nouveauSalaire).isEqualTo(employe.getSalaire());
-    }
-
-    @Test
-    @DisplayName("Test AugmenterSalaire avec salaire multiplié par 4 soit pourcentage de 300%")
-    public void testAugmenterSalaireFois4() throws Exception {
-
-        //Given
-        Employe employe = new Employe();
-        employe.setSalaire(1000.0);
-
-        //When
-        //Integer nbAnnees = employe.getNombreAnneeAnciennete();
-        Double nouveauSalaire = employe.augmenterSalaire( 3.0 );
-        //Double pourcentage=-0.5;
-        //Then
-        //Assertions.assertThat(nbAnnees).isEqualTo(2);
-        Assertions.assertThat(nouveauSalaire).isEqualTo(4000.0);
-    }
+    //Tests unitaires classiques -----> ANNEE ANCIENNETE
 
     //Employé dateEmbauche avec date 2 ans avant aujourd'hui =>
     //2 années d'ancienneté
@@ -107,11 +73,10 @@ public class EmployeTest {
         Assertions.assertThat(nbAnnees).isEqualTo(0);
 
     }
-
+    // Tests paramétrés ---------> PRIME ANNUELLE
     @ParameterizedTest
     @CsvSource({
             "1, 'T12345', 0, 1.0, 1000.0",
-
             "1, 'T12345', 0, 0.5, 500.0",
             "1, 'M12345', 0, 1.0, 1700.0",
             "2, 'T12345', 0, 1.0, 2300.0",
@@ -137,5 +102,70 @@ public class EmployeTest {
         //Then
         Assertions.assertThat(result).isEqualTo(prime);
     }
+    @Test
+    @DisplayName("Test AugmenterSalaire avec pourcentage négatif => salaire identique")
+    public void testAugmenterSalairePourcentageNegatif() throws Exception {
+
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1000.0);
+
+        //When
+        //Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        Double nouveauSalaire = employe.augmenterSalaire(-0.5);
+        //Double pourcentage=-0.5;
+        //Then
+        //Assertions.assertThat(nbAnnees).isEqualTo(2);
+        Assertions.assertThat(nouveauSalaire).isEqualTo(employe.getSalaire());
+    }
+
+    @Test
+    @DisplayName("Test AugmenterSalaire avec salaire multiplié par 4 soit pourcentage de 300%")
+    public void testAugmenterSalaireFois4() throws Exception {
+
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1000.0);
+
+        //When
+        //Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        Double nouveauSalaire = employe.augmenterSalaire(3.0);
+        //Double pourcentage=-0.5;
+        //Then
+        //Assertions.assertThat(nbAnnees).isEqualTo(2);
+        Assertions.assertThat(nouveauSalaire).isEqualTo(4000.0);
+    }
+
+    @Test
+    public void testAugmenterSalaireSiPourcentageA0() throws EmployeException {
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1521.22);
+        //When
+        double result = employe.augmenterSalaire(0.0);
+        //Then
+        Assertions.assertThat(result).isEqualTo(1521.22);
+    }
+    @Test
+    public void testAugmenterSalaireNormal() throws EmployeException {
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1000.0);
+        //When
+        double result = employe.augmenterSalaire(0.5);
+        //Then
+        Assertions.assertThat(result).isEqualTo(1500);
+    }
+    @Test
+    public void testAugmenterSalaireAugmentationTropElevee() throws EmployeException {
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1000.0);
+        //When
+        double result = employe.augmenterSalaire(10);
+        //Then
+        Assertions.assertThat(result).isEqualTo(11000);
+    }
+
 }
 
--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -2,19 +2,19 @@ package com.ipiecoles.java.java350.service;
 
 import com.ipiecoles.java.java350.exception.EmployeException;
 import com.ipiecoles.java.java350.model.Employe;
-import com.ipiecoles.java.java350.model.Entreprise;
 import com.ipiecoles.java.java350.model.NiveauEtude;
 import com.ipiecoles.java.java350.model.Poste;
 import com.ipiecoles.java.java350.repository.EmployeRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.time.LocalDate;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,7 +24,8 @@ class EmployeServiceTest {
     @InjectMocks
     EmployeService employeService;
     @ParameterizedTest()
-    //performance,caTraite, objectifCa, performanceMoyenne,  resultatPerformance
+    // performance , caTraite , objectifCa, performanceMoyenne , resultatPerformance
+
     @CsvSource({
             "2, 70, 100, 1.00, 1",
             "10, 90, 100, 1.00, 9",
@@ -36,17 +37,11 @@ class EmployeServiceTest {
     public void testCalculPerformenceCommercial(Integer performance, Long caTraite,
                                                 Long objectifCa, Double performanceMoyenne,
                                                 Integer resultatPerformance) throws EmployeException {
-
         //Given
         String matricule = "C54321";
-
         Employe employe = new Employe();
         employe.setMatricule(matricule);
         employe.setPerformance(performance);
-        //String matricule="C22222";
-        //Double performance=10.0;
-        //Long caTraite=10000L;
-        //Long objectifCa=50000L;
         Mockito.when(employeRepository.findByMatricule(matricule)).thenReturn(employe);
         Mockito.when(employeRepository.avgPerformanceWhereMatriculeStartsWith("C")).thenReturn(performanceMoyenne);
 
@@ -58,11 +53,10 @@ class EmployeServiceTest {
         Mockito.verify(employeRepository, Mockito.times(1))
                 .save(employeCaptor.capture());
 
-        Employe e = employeCaptor.getValue();
-        //Assertions.assertThat(employe.getMatricule()).isEqualTo("C22222");
-        Assertions.assertThat(employeCaptor.getValue().getPerformance()).isEqualTo(resultatPerformance);
+        Employe employeCaptorValue = employeCaptor.getValue();
+        Assertions.assertThat(employeCaptorValue.getPerformance()).isEqualTo(resultatPerformance);
     }
-
+    // Tests mockés ----------> Embauche
     @Test
     public void testEmbaucheEmployeCommercialPleinTempsBTS() throws EmployeException {
 
@@ -72,53 +66,32 @@ class EmployeServiceTest {
         Poste poste = Poste.COMMERCIAL;
         NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
         Double tempsPartiel = 1.0;
-        // on donne les cas car on n'a pas de BDD donc les données sont apportées avec when
-        // cas nominal, findLastMatricule => 00345 / null
         Mockito.when(employeRepository.findLastMatricule()).thenReturn("00345");
-        // findByMatricule => null / pensez à incrémenter d'1 par rapport au test du dessus 00345, et je veux que ça renvoi null
         Mockito.when(employeRepository.findByMatricule("C00346")).thenReturn(null);
 
         // When
         employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
 
-
         // Then
-        // BDD (simulée) si l'employé est bien créé (nom, prenom, matricule, salaire, date embauche, performance, temps partiel)
-
-        // au moment où on appele save, je récupère les valeur d'employe car je ne peux pas enregistrer en BDD, voir Mock2 cours
-        // initialisation des capteurs d'argument
         ArgumentCaptor<Employe> employeCaptor = ArgumentCaptor.forClass(Employe.class);
-
         Mockito.verify(employeRepository, Mockito.times(1)).save(employeCaptor.capture());
-        // ou
-        // Mockito.verify(employeRep).save(employeCaptor.capture());
-        // ou si on n'est pas passé
-        // Mockito.verify(employeRep, Mockito.never).save(employeCaptor.capture());
 
-        Employe employe = employeCaptor.getValue();
+        // ouMockito.verify(employeRep, Mockito.never).save(employeCaptor.capture());
 
-        // Employe employeVerif = new Employe(nom, prenom..);
-        // Assertions.assertThat(employe).isEqualTo(employeVerif);
-
-        Assertions.assertThat(employe.getNom()).isEqualTo(nom);
-        Assertions.assertThat(employe.getPrenom()).isEqualTo(prenom);
-        Assertions.assertThat(employe.getMatricule()).isEqualTo("C00346");
-
-        Assertions.assertThat(employe.getDateEmbauche()).isEqualTo(LocalDate.now());
+        Employe employeCaptorValue = employeCaptor.getValue();
+        Assertions.assertThat(employeCaptorValue.getNom()).isEqualTo(nom);
+        Assertions.assertThat(employeCaptorValue.getPrenom()).isEqualTo(prenom);
+        Assertions.assertThat(employeCaptorValue.getMatricule()).isEqualTo("C00346");
+        Assertions.assertThat(employeCaptorValue.getDateEmbauche()).isEqualTo(LocalDate.now());
         // ou
         //Assertions.assertThat(employeCaptor.getValue().getDateEmbauche().format(
         //		DateTimeFormatter.ofPattern("yyyyMMdd"))).isEqualTo(LocalDate.now().format(
         //				DateTimeFormatter.ofPattern("yyyyMMdd")));
-
-        Assertions.assertThat(employe.getTempsPartiel()).isEqualTo(tempsPartiel);
-
+        Assertions.assertThat(employeCaptorValue.getTempsPartiel()).isEqualTo(tempsPartiel);
         // Assertions.assertThat(employe.getPerformance()).isEqualTo(Entreprise.PERFORMANCE_BASE);
-        Assertions.assertThat(employe.getPerformance()).isEqualTo(1);
-
+        Assertions.assertThat(employeCaptorValue.getPerformance()).isEqualTo(1);
         // salaire de base 1521 * 1.2 * 1.0 = 1825.46
-        Assertions.assertThat(employe.getSalaire()).isEqualTo(1825.46);
-
-
+        Assertions.assertThat(employeCaptorValue.getSalaire()).isEqualTo(1825.46);
     }
 
     @Test
@@ -131,21 +104,17 @@ class EmployeServiceTest {
         NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
         Double tempsPartiel = 1.0;
 
-        // je simule que la base arrive à terme
         Mockito.when(employeRepository.findLastMatricule()).thenReturn("99999");
 
         // When
         try {
             employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
-            Assertions.fail("Aurait dû planter!"); // fail fait échourer le test qd même et signale que ça aurait du planter
+            Assertions.fail("Aurait dû planter!");
         } catch (Exception e) {
             // Then
-            // je fais en sorte que j'ai bien une exception avec le try catch dans when
             Assertions.assertThat(e).isInstanceOf(EmployeException.class);
             Assertions.assertThat(e.getMessage()).isEqualTo("Limite des 100000 matricules atteinte !");
         }
-
     }
-
 
 }
