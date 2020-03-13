7d81ab5b74820b7daf5073f5690f7e47aa45355d
bordegael <bordegael@hotmail.fr>
2020-03-13 09:41:17 +0100

    tp

---
+++ src/test/java/com/ipiecoles/java/java350/acceptance/EmployeAcceptanceTest.java
@@ -0,0 +1,8 @@
+package com.ipiecoles.java.java350.acceptance;
+
+public class EmployeAcceptanceTest {
+    @Step("J'embauche une personne appelée <prenom> <nom> diplomée d'un <diplome> en tant que <poste> à <txActivite>")
+    public void embauche(String prenom, String nom, String diplome, String poste, String txActivite){
+
+    }
+}
