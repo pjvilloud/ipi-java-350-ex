7c684603747b0dd344a68b10f060f9dad111d64e
Kevinipi <kevin.pose@ipilyon.net>
2020-03-25 15:31:26 +0100

    Delete for now test getprimeanuelle

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -61,28 +61,4 @@ public class EmployeTest {
         //Then
         Assertions.assertThat(anneeAnciennete).isEqualTo(0);
     }
-
-    @ParameterizedTest
-    @CsvSource({
-            "1, 'T12345', 0, 1.0, 1000.0",
-            "1, 'T12345', 2, 0.5, 600.0",
-            "1, 'T12345', 2, 1.0, 1200.0",
-            "2, 'T12345', 0, 1.0, 2300.0",
-            "2, 'T12345', 1, 1.0, 2400.0",
-            "1, 'M12345', 0, 1.0, 1700.0",
-            "1, 'M12345', 5, 1.0, 2200.0",
-            "2, 'M12345', 0, 1.0, 1700.0",
-            "2, 'M12345', 8, 1.0, 2500.0"
-    })
-    public void getPrimeAnnuelle(Integer performance, String matricule, Long nbYearsAnciennete, Double tempsPartiel, Double primeAnnuelle){
-        //Given
-        Employe employe = new Employe("Doe", "John", matricule, LocalDate.now().minusYears(nbYearsAnciennete), Entreprise.SALAIRE_BASE, performance, tempsPartiel);
-
-        //When
-        Double prime = employe.getPrimeAnnuelle();
-
-        //Then
-        Assertions.assertThat(primeAnnuelle).isEqualTo(0);
-
-    }
 }
