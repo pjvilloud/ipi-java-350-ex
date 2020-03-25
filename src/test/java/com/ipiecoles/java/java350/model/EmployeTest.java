86f7ee08faa90156456b8b091b51ec887998235b
Tomy-sama <tom_nguyen0@hotmail.fr>
2020-03-25 14:03:57 +0100

    travis8 EMPLOYEJAVA

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,37 @@
+package com.ipiecoles.java.java350.model;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeTest {
+
+    @Test
+    public void test() {
+
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void getNombreAnneeAnciennete() {
+        
+    }
+
+    @Test
+    void getNbConges() {
+    }
+
+    @Test
+    void getNbRtt() {
+    }
+
+    @Test
+    void testGetNbRtt() {
+    }
+
+    @Test
+    void getPrimeAnnuelle() {
+    }
+}
