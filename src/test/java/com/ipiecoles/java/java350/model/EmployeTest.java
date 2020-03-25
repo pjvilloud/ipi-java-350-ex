408b69914c80d2ceee6b13d344a7b2afd67b2352
hamidi <johanhamididu38@gmail.com>
2020-03-25 16:53:46 +0100

    test unitaire paramétré

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java350.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -9,6 +11,7 @@ import java.util.Date;
 
 public class EmployeTest {
 
+    //For method getNombreAnneeAnciennete()
     @Test
     public void testNbAnneeAncienneteNow(){
         //Given = Initialisation des données d'entrée
@@ -60,4 +63,29 @@ public class EmployeTest {
         //Then = Vérifications de ce qu'a fait la méthode
         Assertions.assertThat(nbAnnees).isEqualTo(0);
     }
+
+    //For method getPrimeAnnuelle()
+    @ParameterizedTest
+    @CsvSource({
+            "'C12345', 1.0, 0, 1, 1000.0",
+            "'M12345', 1.0, 0, 1, 1700.0",
+            "'M12345', 0.5, 0, 1, 500.0",
+            "'C12345', 1.0, 0, 2, 2300.0",
+
+    })
+    public void testGetPrimeAnnuelle(String matricule, Double tempsPartiel, Integer nbAnneeAnciennete, Integer perfomance, Double primeCalculee){
+        //Given = Initialisation des données d'entrée
+        Employe employe = new Employe();
+        employe.setMatricule(matricule);
+        employe.setTempsPartiel(tempsPartiel);
+        employe.setDateEmbauche(LocalDate.now().minusYears(nbAnneeAnciennete));
+        employe.setPerformance(perfomance);
+
+        //When = Exécution de la méthode à tester
+        Double prime = employe.getPrimeAnnuelle();
+
+        //Then = Vérifications de ce qu'a fait la méthode
+        Assertions.assertThat(prime).isEqualTo(primeCalculee);
+    }
+
 }
