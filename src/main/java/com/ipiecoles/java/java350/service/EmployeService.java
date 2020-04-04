578b134c7992c5610a979bc9ee10dade39b96326
Kevinipi <kevin.pose@ipilyon.net>
2020-04-04 17:17:33 +0200

    Suppress import Test because is never used on this file

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.testng.annotations.Test;
 
 import javax.persistence.EntityExistsException;
 import java.time.LocalDate;
