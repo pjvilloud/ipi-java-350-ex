51bc171e376c6d33415f0f8c25381b7d9b03f9bb
Kevinipi <kevin.pose@ipilyon.net>
2020-04-05 16:18:41 +0200

    Delete unused package

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -1,11 +1,9 @@
 package com.ipiecoles.java.java350.model;
-import com.beust.jcommander.Parameter;
 import com.ipiecoles.java.java350.service.EmployeService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.runner.RunWith;
 
 import java.time.LocalDate;
 
