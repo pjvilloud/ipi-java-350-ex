30ea8f37b3df29d16d9a209ca6de37d44687188a
Kevinipi <kevin.pose@ipilyon.net>
2020-04-05 10:43:53 +0200

    Coverage getNbRtt()

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,14 +2,26 @@ package com.ipiecoles.java.java350.model;
 import com.ipiecoles.java.java350.service.EmployeService;
 import io.cucumber.java8.En;
 import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.testng.asserts.Assertion;
 
 import java.time.LocalDate;
 
 public class EmployeTest {
     private EmployeService employeService;
+
+    @Test
+    public void getNbRtt(){
+        //Given
+        Employe employe = new Employe();
+        //When
+        Integer nbRTT = employe.getNbRtt();
+        //Then
+        Assertions.assertThat(nbRTT).isEqualTo(9);
+    }
 
     @Test
     public void getNbConges(){
