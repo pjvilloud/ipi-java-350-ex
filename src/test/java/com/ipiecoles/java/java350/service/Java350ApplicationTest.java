f75e68690293c3abbe9b436838f3d0318c68163d
Kevinipi <kevin.pose@ipilyon.net>
2020-04-05 16:39:47 +0200

    Coverage Java350application

---
+++ src/test/java/com/ipiecoles/java/java350/service/Java350ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ipiecoles.java.java350.service;
+import com.ipiecoles.java.java350.Java350Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+
+public class Java350ApplicationTest {
+
+    @Test
+    public void main(){
+        SpringApplication.run(Java350Application.class);
+    }
+}
