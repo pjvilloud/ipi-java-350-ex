0216af565840efa4f7d7daf72c6c44e5a9695950
Kevinipi <kevin.pose@ipilyon.net>
2020-04-07 15:42:03 +0200

    Ajout @springBootTest file Java350ApplicationTest

--- src/test/java/com/ipiecoles/java/java350/service/Java350ApplicationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/Java350ApplicationTest.java
@@ -1,8 +1,11 @@
 package com.ipiecoles.java.java350.service;
 import com.ipiecoles.java.java350.Java350Application;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class Java350ApplicationTest {
 
     @Test
