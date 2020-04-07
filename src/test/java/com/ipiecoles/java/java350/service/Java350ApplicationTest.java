fbfa604607bcf9b5dac8b7a82385d15ed158fac1
Kevinipi <kevin.pose@ipilyon.net>
2020-04-07 10:57:03 +0200

    delete unused package import

--- src/test/java/com/ipiecoles/java/java350/service/Java350ApplicationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/Java350ApplicationTest.java
@@ -1,10 +1,7 @@
 package com.ipiecoles.java.java350.service;
 import com.ipiecoles.java.java350.Java350Application;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.testng.asserts.Assertion;
 
 public class Java350ApplicationTest {
 
