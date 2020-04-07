9942939dcf7a7474c40ce7a1b5964cbce5d695c9
Tomy-sama <tom_nguyen0@hotmail.fr>
2020-04-07 14:19:52 +0200

    EmployeRepoTest modifié pour qu'il marche

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -1,16 +1,17 @@
 package com.ipiecoles.java.java350.repository;
 
 import com.ipiecoles.java.java350.model.Employe;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.*;
+
 
 import java.time.LocalDate;
 
 
-@SpringBootTest
+@DataJpaTest
 class EmployeRepositoryTest {
 
     @Autowired
@@ -28,20 +29,20 @@ class EmployeRepositoryTest {
         // When
         String lastMatricule = employeRepository.findLastMatricule();
         // Then
-        Assertions.assertThat(lastMatricule).isNull();
+        Assertions.assertNull(lastMatricule);
     }
     @Test
-    void findLastMatricule3Employes() {
+    void testFindLastMatricule3Employes() {
         // Given
-        Employe e1 = new Employe("Doe", "John", "T12345",
-                LocalDate.now(), 200d, 1 , 1.0);
-        Employe e2 = new Employe("Doe", "Jane", "C67890",
-                LocalDate.now(), 200d, 1 , 1.0);
-        Employe e3 = new Employe("Doe", "Jane", "M45678",
-                LocalDate.now(), 200d, 1 , 1.0);
-        // When
+        employeRepository.save(new Employe("Doe", "John", "T12345",
+                LocalDate.now(), 2000d, 1 , 1.0));
+        employeRepository.save(new Employe("Doe", "Jane", "C67890",
+                LocalDate.now(), 2000d, 1 , 1.0));
+        employeRepository.save(new Employe("Doe", "Jane", "M45678",
+                LocalDate.now(), 2000d, 1 , 1.0));
+        //When
         String lastMatricule = employeRepository.findLastMatricule();
-        // Then
-        Assertions.assertThat(lastMatricule).isEqualTo("67890");
+        //Then
+        Assertions.assertEquals("67890", lastMatricule);
     }
 }
