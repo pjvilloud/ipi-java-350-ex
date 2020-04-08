db1a1f608e3d5b5ea5e9a2da39b1607a4190fe6c
mathis69400 <45007491+mathis69400@users.noreply.github.com>
2020-04-08 15:33:39 +0200

    Update Employe.java

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -63,14 +63,20 @@ public class Employe {
     }
 
     public Integer getNbRtt(LocalDate d){
-        int i1 = d.isLeapYear() ? 365 : 366;int var = 104;
+        int i1 = d.isLeapYear() ? 365 : 366;
+        int var = 104;
         switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
-        case THURSDAY: if(d.isLeapYear()) var =  var + 1; break;
-        case FRIDAY:
-        if(d.isLeapYear()) var =  var + 2;
-        else var =  var + 1;
-case SATURDAY:var = var + 1;
-                    break;
+            case THURSDAY:
+                if(d.isLeapYear())
+                    var =  var + 1;
+                break;
+            case FRIDAY:
+                if(d.isLeapYear()) var =  var + 2;
+                else var =  var + 1;
+                break;
+            case SATURDAY:
+                var = var + 1;
+                break;
         }
         int monInt = (int) Entreprise.joursFeries(d).stream().filter(localDate ->
                 localDate.getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue()).count();
