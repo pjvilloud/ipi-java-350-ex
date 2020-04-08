1d06cd95d732e04e6abf38891e3cefbf8dc0ef7b
Kevinipi <kevin.pose@ipilyon.net>
2020-04-08 14:24:47 +0200

    modify Java350ApplicationTest

--- src/test/java/com/ipiecoles/java/java350/service/Java350ApplicationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/Java350ApplicationTest.java
@@ -1,19 +1,13 @@
 package com.ipiecoles.java.java350.service;
 import com.ipiecoles.java.java350.Java350Application;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest(classes = Java350Application.class)
 public class Java350ApplicationTest {
 
     @Test
-    public void testmain(){
-        Java350Application.main(new String[]{
-                "--spring.main.web-environment=false",
-                "--spring.autoconfigure.exclude=blahblahblah",
-                // Override any other environment properties according to your needs
-        });
+    public void testMain(){
+        Java350Application.main(new String[]{});
     }
 }
