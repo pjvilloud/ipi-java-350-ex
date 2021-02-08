c2af5894b52aab50920daa938e86a4d59d1082c0
pitchounvivi <pitchounvivi@gmail.com>
2021-02-08 15:59:32 +0100

    préparation d'un test préparé

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -59,4 +59,37 @@ public class EmployeTest {
         Assertions.assertThat(anneeAnciennete).isEqualTo(0);
     }
 
+
+    //////////////////////////////////////Tests préparés
+    //Pour la méthode getPrimeAnnuelle()
+    //1) les entrées
+    //date d'embauche (car la prime n'est pas une entrée en tant que tel)
+    //indice manager
+    //indice performance
+    //temps partiel (% d'activité)
+
+    //2) scénario faisant varier les entrées
+    //employé simple sans ancienneté
+    //employé simple avec ancienneté
+    //employé avec performance
+    //employé est un manager/technicien sont gérés de la même manière
+    //employé est à temps partiel
+
+    //3) écrire un TU simple qui teste un des scénarios
+
+
+    //4) dupliquer et transformer le scénario en test préparé testant tous les scénarios
+
+
+    @Test //version TU simple
+    public void testGetPrimeAnnuelle(){
+
+
+
+
+
+    }
+
+
+
 }
