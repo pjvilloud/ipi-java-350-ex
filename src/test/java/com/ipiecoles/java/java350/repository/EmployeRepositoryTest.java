97853bfbc45a34a028151533ffc06fdc7ff6ad5b
pitchounvivi <pitchounvivi@gmail.com>
2021-02-09 13:11:03 +0100

    correction du TU findByMatricule

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -12,16 +12,17 @@ import java.time.LocalDate;
 //@ExtendWith(SpringExtension.class) //Junit 5
 //@DataJpaTest  // ou @SpringBootTest ==> cette ligne suffit pour tester le repo tout seul (mais il faut aussi celle du dessus)
 //@ContextConfiguration(classes = {Java350Application.class}) //Pour définir le contexte de l'appli (spécifique pour l'appli (car il y a son nom))
-@SpringBootTest //==> dans le cas ou on utilise Spring Boot seule cette ligne suffit
+
+@SpringBootTest //==> dans le cas ou on utilise Spring Boot seule cette ligne suffit (pour définir le context)
 class EmployeRepositoryTest {
 
     //on importe la classe pour la tester
     @Autowired
     EmployeRepository employeRepository;
 
-    //TU pour tester le dernier numéro de matricule enregistré en bdd
+    //TU (<= même si on fait un peu d'insertion)  pour tester le dernier numéro de matricule enregistré en bdd
     @Test
-    public void testFindLastMatricule(){
+    public void testFindLastMatriculeUnEmploye(){
         //given
         //insert des données en base
         employeRepository.save(new Employe("Doe", "John", "T012345", LocalDate.now(), 1500d, 1, 1.0));
@@ -31,9 +32,13 @@ class EmployeRepositoryTest {
 
         //when
         //execute la requête dans la bdd
-        String lastMatricule = employeRepository.findLastMatricule();
+        String lastMatricule = employeRepository.findLastMatricule(); //la méthode enlève la lettre pour ne donner que le nombre
 
         //Then
-        Assertions.assertThat(lastMatricule).isEqualTo("345"); //le Test enlevé lors de l'insertion
+        Assertions.assertThat(lastMatricule).isEqualTo("345");
     }
+
+
+
+
 }
