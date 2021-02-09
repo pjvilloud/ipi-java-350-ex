d32b879b8f59763fdbf874db2fc71e01b82750c4
pitchounvivi <pitchounvivi@gmail.com>
2021-02-09 11:25:24 +0100

    complément commentaires

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -1,15 +1,19 @@
 package com.ipiecoles.java.java350.repository;
 
 
+import com.ipiecoles.java.java350.Java350Application;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//les 2 lignes @ chargent les données du context, c'est indispensable pour faire les tests
-@ExtendWith(SpringExtension.class) //Junit 5
-@DataJpaTest  // ou @SpringBootTest ==> cette ligne suffit pour tester le repo tout seul
+//@ExtendWith(SpringExtension.class) //Junit 5
+//@DataJpaTest  // ou @SpringBootTest ==> cette ligne suffit pour tester le repo tout seul
+//@ContextConfiguration(classes = {Java350Application.class}) //Pour définir le contexte de l'appli (spécifique pour l'appli (car il y a son nom))
+@SpringBootTest //==> dans le cas ou on utilise Spring Boot
 class EmployeRepositoryTest {
 
     //on importe la classe pour la tester
