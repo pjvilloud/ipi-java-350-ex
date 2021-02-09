3915d6c401b8639ecf5d5dd8a24c62a3af75dff9
pitchounvivi <pitchounvivi@gmail.com>
2021-02-09 11:08:28 +0100

    ajout commentaire explicatif concernant les tests à effectuer

--- src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
+++ src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
@@ -9,11 +9,15 @@ import java.util.List;
 
 @Repository
 public interface EmployeRepository extends JpaRepository<Employe, Long> {
+
+    //on teste par contre toutes les méthodes écrites manuellement donc celle-ci
     @Query("select max(substring(matricule,2)) from Employe")
     String findLastMatricule();
 
+    //Cette méthode n'a pas besoin d'être testée, car c'est une méthode native = générée par SpringData
     Employe findByMatricule(String matricule);
 
+    //et cette méthode là
     @Query("select avg(performance) from Employe where SUBSTRING(matricule,0,1) = ?1 ")
     Double avgPerformanceWhereMatriculeStartsWith(String premiereLettreMatricule);
 }
