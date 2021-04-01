c7f8b7c32376cc4d9d29ac82c38e4f5aebd35689
Eymeric Sertgoz <eymeric.sertgoz@gmail.com>
2021-04-01 14:24:39 +0200

    Tests unitaires : tests paramétré sur 'getPrimeAnnuelle'

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java350.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 
@@ -90,4 +92,34 @@ public class EmployeTest {
         Assertions.assertThat(nbAnneeAnciennete).isZero();
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "1, 'T12345', 0, 1.0, 1000.0",
+            "1, 'T12345', 2, 0.5, 600.0",
+            "1, 'T12345', 2, 1.0, 1200.0",
+            "2, 'T12345', 0, 1.0, 2300.0",
+            "2, 'T12345', 1, 1.0, 2400.0",
+            "1, 'M12345', 0, 1.0, 1700.0",
+            "1, 'M12345', 5, 1.0, 2200.0",
+            "2, 'M12345', 0, 1.0, 1700.0",
+            "2, 'M12345', 8, 1.0, 2500.0"
+    })
+    public void testGetPrimeAnnuelle(Integer performance, String matricule, Long nbYearsAnciennete, Double tempsPartiel, Double primeAnnuelle){
+        //Given
+        Employe employe = new Employe(
+                "Doe",
+                "John",
+                matricule,
+                LocalDate.now().minusYears(nbYearsAnciennete),
+                Entreprise.SALAIRE_BASE,
+                performance,
+                tempsPartiel
+        );
+
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertThat(prime).isEqualTo(primeAnnuelle);
+    }
 }
