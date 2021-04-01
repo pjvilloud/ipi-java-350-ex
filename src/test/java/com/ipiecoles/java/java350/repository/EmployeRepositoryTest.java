7f40e0aea56860c80d8cfa85bd3c4a02da451a98
Eymeric Sertgoz <eymeric.sertgoz@gmail.com>
2021-04-01 15:59:09 +0200

    Tests d'intégration : tests de repo sur 'findLastMatricule'

---
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.ipiecoles.java.java350.repository;
+
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class EmployeRepositoryTest {
+
+    @Autowired
+    EmployeRepository employeRepository;
+
+    @BeforeEach
+    @AfterEach
+    public void setup() {
+        employeRepository.deleteAll();
+    }
+
+    @Test
+    public void testFindLastMatriculeIsEmpty(){
+        //Given
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //Then
+        Assertions.assertThat(lastMatricule).isEqualTo(null);
+    }
+
+    @Test
+    public void testFindLastMatricule(){
+        //Given
+        employeRepository.save(new Employe("Doe", "John", "T12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //Then
+        Assertions.assertThat(lastMatricule).isEqualTo("12345");
+    }
+
+    @Test
+    public void testFindLastMatriculeMultiple(){
+        //Given
+        employeRepository.save(new Employe("Doe", "John", "T12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+        employeRepository.save(new Employe("Doe", "Jane", "M40325", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+        employeRepository.save(new Employe("Doe", "Jim", "C06432", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //Then
+        Assertions.assertThat(lastMatricule).isEqualTo("40325");
+    }
+}
