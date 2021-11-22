51ce998a8845a626eb1b5d24cc4d4bcc8fe21882
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-11-22 17:03:18 +0100

    ;

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,6 +2,9 @@ package com.ipiecoles.java.java350.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 
@@ -56,4 +59,18 @@ class EmployeTest {
         //Then
         Assertions.assertThat(nbAnnees).isEqualTo(0);
     }
+    @ParameterizedTest
+    @CsvSource({
+            "'M12345',0,1,1.0,1700.0"
+    })
+    void getPrimeAnnuelle (String Matricule, Integer nbAnneeAnciennete, Integer performance, Double TempsPartiel, Double PrimeCalculee) {
+        //Given
+        //Matricule, Date embauche,
+        Employe employe = new Employe("Doe", "Joe", "M12345", LocalDate.now(), 2500.0, 1,1.0);
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+        //Then
+        Assertions.assertThat(prime).isEqualTo(PrimeCalculee);
+    }
+
 }
