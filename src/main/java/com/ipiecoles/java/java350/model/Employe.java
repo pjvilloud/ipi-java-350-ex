12164bf3fe5222c9a4b895edff0e85f758b2de5b
Charlene <martel.pereira@gmail.com>
2021-12-21 10:53:27 +0100

    Journal

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -228,4 +228,13 @@ case SATURDAY:var = var + 1;
     public int hashCode() {
         return Objects.hash(id, nom, prenom, matricule, dateEmbauche, salaire, performance);
     }
+
+    @Service public class MyServiceImpl implements MyService {
+        private static final Logger LOGGER = LoggerFactory.getLogger(MyServiceImpl.class);
+        @Override public void doStuff(final String value) {
+            LOGGER.trace("doStuff needed more information - {}", value);
+            LOGGER.debug("doStuff needed to debug - {}", value);
+            LOGGER.info("doStuff took input - {}", value);
+            LOGGER.warn("doStuff needed to warn - {}", value);
+            LOGGER.error("doStuff encountered an error with value - {}", value);
 }
