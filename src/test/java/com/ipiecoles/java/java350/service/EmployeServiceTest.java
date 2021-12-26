890e9a8a9bece1ffc313cc70fae2925609d224ce
marinededola <72435141+marinededola@users.noreply.github.com>
2021-12-26 16:31:02 +0100

    Tests méthode calculPerformanceCommercial

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -21,6 +21,11 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.*;
+
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -140,5 +145,147 @@ public class EmployeServiceTest {
         //When/Then
         EmployeException e = Assertions.assertThrows(EmployeException.class, () -> employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel));
         Assertions.assertEquals("Limite des 100000 matricules atteinte !", e.getMessage());
+    }
+
+    @Test
+    void testCalculPerformanceCommercialeCaTraiteNull() {
+        //GIVEN
+        employeRepository.save(new Employe("Doe", "John", "C06432", LocalDate.now(), 1825.464, 1, 1.0));
+        String matricule = "C06432";
+        Long caTraite = null;
+        Long objectifCa = 500L;
+        //WHEN
+        try {
+            employeService.calculPerformanceCommercial(matricule,caTraite,objectifCa);
+            Assertions.fail("calculPerformanceCommercial aurait dû lancer une exception");
+        } catch (EmployeException e) {
+            //THEN
+            org.assertj.core.api.Assertions.assertThat(e.getMessage()).isEqualTo("Le chiffre d'affaire traité ne peut être négatif ou null !");
+        }
+    }
+
+    @Test
+    void testCalculPerformanceCommercialeCaTraiteNegatif() {
+
+        //GIVEN
+        employeRepository.save(new Employe("Doe", "John", "C06432", LocalDate.now(), 1825.464, 1, 1.0));
+        String matricule = "C06432";
+        Long caTraite = -500L;
+        Long objectifCa = 500L;
+
+        //WHEN
+        try {
+            employeService.calculPerformanceCommercial(matricule,caTraite,objectifCa);
+            Assertions.fail("calculPerformanceCommercial aurait dû lancer une exception");
+        } catch (EmployeException e) {
+            //THEN
+            org.assertj.core.api.Assertions.assertThat(e.getMessage()).isEqualTo("Le chiffre d'affaire traité ne peut être négatif ou null !");
+        }
+    }
+
+
+    @Test
+    void testCalculPerformanceCommercialeObjectifCaNull() {
+        //GIVEN
+        employeRepository.save(new Employe("Doe", "John", "C06432", LocalDate.now(), 1825.464, 1, 1.0));
+        String matricule = "C06432";
+        Long caTraite = 500L;
+        Long objectifCa = null;
+        //WHEN
+        try {
+            employeService.calculPerformanceCommercial(matricule,caTraite,objectifCa);
+            Assertions.fail("calculPerformanceCommercial aurait dû lancer une exception");
+        } catch (EmployeException e) {
+            //THEN
+            org.assertj.core.api.Assertions.assertThat(e.getMessage()).isEqualTo("L'objectif de chiffre d'affaire ne peut être négatif ou null !");
+        }
+    }
+
+    @Test
+    void testCalculPerformanceCommercialeObjectifCaNegatif() {
+
+        //GIVEN
+        employeRepository.save(new Employe("Doe", "John", "C06432", LocalDate.now(), 1825.464, 1, 1.0));
+        String matricule = "C06432";
+        Long caTraite = 500L;
+        Long objectifCa = -500L;
+
+        //WHEN
+        try {
+            employeService.calculPerformanceCommercial(matricule,caTraite,objectifCa);
+            Assertions.fail("calculPerformanceCommercial aurait dû lancer une exception");
+        } catch (EmployeException e) {
+            //THEN
+            org.assertj.core.api.Assertions.assertThat(e.getMessage()).isEqualTo("L'objectif de chiffre d'affaire ne peut être négatif ou null !");
+        }
+    }
+
+
+    @Test
+    void testCalculPerformanceCommercialeMatriculeDontExist() {
+        //GIVEN
+        String matricule = "C35353";
+
+        Long caTraite = 500L;
+        Long objectifCa = 500L;
+
+        //WHEN
+        try {
+            employeService.calculPerformanceCommercial(matricule,caTraite, objectifCa);
+            Assertions.fail("La méthode calculPerformanceCommercial doit lancer une exception");
+        } catch (EmployeException e) {
+            //THEN
+            org.assertj.core.api.Assertions.assertThat(e.getMessage()).isEqualTo("Le matricule C35353 n'existe pas !");
+        }
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "T00001",
+            ", ,"
+    })
+    void testCalculPerformanceCommercialMatriculeNullOrNotBeginWithCOrDontExist(String matricule) {
+
+        //GIVEN
+        Employe employe = new Employe("Doe", "Joe", matricule, LocalDate.now(), 1500d, 1, 1.0);
+        Long caTraite = 1000L;
+        Long objectifCa = 1000L;
+        //WHEN
+        try {
+            employeService.calculPerformanceCommercial(employe.getMatricule(),caTraite, objectifCa);
+            Assertions.fail("La méthode calculPerformanceCommercial doit lancer une exception");
+        } catch (EmployeException e) {
+            //THEN
+            org.assertj.core.api.Assertions.assertThat(e.getMessage()).isEqualTo("Le matricule ne peut être null et doit commencer par un C !");
+
+        }
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 600, 1000, 2", //Cas 1 / Autres
+            "4, 10, 100, 2", //Cas 2
+            "3, 1000, 1000, 4", //Cas 3
+            "4, 720, 600, 6", //Cas 4
+            "5, 10000, 600, 10", //Cas 5
+    })
+    void testCalculPerformanceCommerciale(Integer performanceInitiale, Long caTraite, Long objectifCa,  Integer performanceAttendue) throws EmployeException {
+
+        //GIVEN
+        Employe employe;
+        String matricule  = "C06432";
+
+        Mockito.when(employeRepository.findByMatricule(matricule)).thenReturn(
+                employe = new Employe("Doe", "Joe", matricule, LocalDate.now(), 160d, performanceInitiale, 1.0)
+        );
+
+        //WHEN
+        employeService.calculPerformanceCommercial(employe.getMatricule(),caTraite, objectifCa);
+        Integer performanceRecupere = employe.getPerformance();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(performanceRecupere).isEqualTo(performanceAttendue);
     }
 }
