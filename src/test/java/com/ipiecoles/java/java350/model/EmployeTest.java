54ef82ebd924c86a2c1511a843c9db5de7be44a0
Jose-Rvlmnnts <ravelomanantsoajose06@gmail.com>
2022-01-06 09:26:30 +0100

    test: TU on calculPrimeAnnuelle

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java350.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 
@@ -51,5 +53,28 @@ class EmployeTest {
 
         //Then = Vérification de ce que fait la méthode
         Assertions.assertEquals(1,nbAnnesAnciennete);
+    }
+
+    @ParameterizedTest(name = "Matricule {0}, performance {1}, anciennete {2}, temps partiel {3} => prime {4}")
+    @CsvSource({
+            "'T12345',1,0,1.0,1000.0",
+            ",1,0,1.0,1000.0",
+            "'T12345',,0,1.0,1000.0",
+            "'M12345',1,0,1.0,1700.0",
+            "'T12345',2,0,1.0,2300.0",
+    })
+    public void testGetPrimeAnnuelle(String matricule, Integer performance,
+                                     Long nbAnneesAnciennete, Double tempsPartiel,
+                                     Double primeAttendue){
+        //Given
+        Employe employe = new Employe();
+        employe.setMatricule(matricule);
+        employe.setPerformance(performance);
+        employe.setDateEmbauche(LocalDate.now().minusYears(nbAnneesAnciennete));
+        employe.setTempsPartiel(tempsPartiel);
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+        //Then
+        Assertions.assertEquals(primeAttendue,prime);
     }
 }
