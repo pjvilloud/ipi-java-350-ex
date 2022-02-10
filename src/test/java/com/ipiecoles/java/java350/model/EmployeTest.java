4b4f42f54160fb9be6d401d09c3466a14e8164ad
Folly Koudouovoh <koudouovohfolly@gmail.com>
2022-02-10 10:01:09 +0100

    Ajout TU sur getPrimeAnnuelle

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -1,7 +1,8 @@
 package com.ipiecoles.java.java350.model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
