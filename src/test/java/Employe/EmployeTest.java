7abfe1b0f225d150e76c6c49d656b398c01e8d55
Roji89 <roja.gh89@gmail.com>
2022-04-26 16:21:37 +0200

    test prime

--- src/test/java/Employe/EmployeTest.java
+++ src/test/java/Employe/EmployeTest.java
@@ -48,7 +48,12 @@ public class EmployeTest {
 
 
     @ParameterizedTest
-    @CsvSource({"'M12345',0,1,1.0,1700.0"    })
+    @CsvSource({
+            "'M12345',0,1,1.0,1700.0",
+            "'T12346',0,1,1.0,1000.0",
+            "'T12346',0,2,1.0,2300.0",
+            ",0,1,1.0,1000.0",
+            "'T12346',0,,1.0,1000.0"})
     public void testGetPrimeAnnuelleManagerPerformanceBasePleinTemps(
             String matricule,
             Integer nbAnneesAnciennete,
