ae8bb84e172f1e2b511ed7c2c70c738d17121604
Roji89 <roja.gh89@gmail.com>
2022-06-20 12:28:27 +0200

    test service

---
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -0,0 +1,56 @@
+package com.ipiecoles.java.java350.service;
+
+import com.ipiecoles.java.java350.exception.EmployeException;
+import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
+import com.ipiecoles.java.java350.model.NiveauEtude;
+import com.ipiecoles.java.java350.model.Poste;
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@SpringBootTest
+public class EmployeServiceTest {
+
+    @Autowired
+    EmployeService employeService;
+
+    @Autowired
+    EmployeRepository employeRepository;
+
+    @AfterEach
+    @BeforeEach
+    public void before(){
+        employeRepository.deleteAll();
+    }
+
+    @Test
+    public void testEmbaucheEmploye() throws EmployeException {
+
+        employeService.embaucheEmploye("Doe", "John", Poste.COMMERCIAL,NiveauEtude.MASTER, 1.0);
+
+
+        Employe employe = employeRepository.findByMatricule("C00001");
+
+        Assertions.assertThat(employe.getNom()).isEqualTo("Doe");
+        Assertions.assertThat(employe.getPrenom()).isEqualTo("John");
+        Assertions.assertThat(employe.getDateEmbauche()).isEqualTo(LocalDate.now());
+        Assertions.assertThat(employe.getPerformance()).isEqualTo(Entreprise.PERFORMANCE_BASE);
+        Assertions.assertThat(employe.getSalaire()).isEqualTo(2129.71);
+        Assertions.assertThat(employe.getTempsPartiel()).isEqualTo(1);
+
+
+    }
+
+
+
+
+
+}
