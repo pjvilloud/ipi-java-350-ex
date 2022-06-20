7f929b8e136659a6ab2f96923fa227c23afea835
Roji89 <roja.gh89@gmail.com>
2022-06-20 14:47:36 +0200

    test: employetest embauche

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -63,7 +63,7 @@ public class EmployeService {
         //Création et sauvegarde en BDD de l'employé.
         Employe employe = new Employe(nom, prenom, matricule, LocalDate.now(), salaire, Entreprise.PERFORMANCE_BASE, tempsPartiel);
 
-        employeRepository.save(employe);
+         employeRepository.save(employe);
 
     }
 
--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -10,34 +10,44 @@ import org.junit.jupiter.api.AfterEach;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityExistsException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class EmployeServiceTest {
 
-    @Autowired
-    EmployeService employeService;
+    @InjectMocks
+    private EmployeService employeService;
 
-    @Autowired
-    EmployeRepository employeRepository;
+    @Mock
+    private EmployeRepository employeRepository;
 
-    @AfterEach
-    @BeforeEach
-    public void before(){
-        employeRepository.deleteAll();
-    }
 
     @Test
     public void testEmbaucheEmploye() throws EmployeException {
 
+        //Givern
+        Mockito.when(employeRepository.findLastMatricule()).thenReturn(null);
+        Mockito.when(employeRepository.findByMatricule("C00001")).thenReturn(null);
+        //main
         employeService.embaucheEmploye("Doe", "John", Poste.COMMERCIAL,NiveauEtude.MASTER, 1.0);
 
+        //Then
+        ArgumentCaptor<Employe> employeCaptor = ArgumentCaptor.forClass(Employe.class);
+        Mockito.verify(employeRepository).save(employeCaptor.capture());
+        //Assertions.assertThat(employeCaptor.getValue().getSalaire()).isEqualTo(2129.71);
 
-        Employe employe = employeRepository.findByMatricule("C00001");
+        Employe employe = employeCaptor.getValue();
 
         Assertions.assertThat(employe.getNom()).isEqualTo("Doe");
         Assertions.assertThat(employe.getPrenom()).isEqualTo("John");
@@ -45,12 +55,35 @@ public class EmployeServiceTest {
         Assertions.assertThat(employe.getPerformance()).isEqualTo(Entreprise.PERFORMANCE_BASE);
         Assertions.assertThat(employe.getSalaire()).isEqualTo(2129.71);
         Assertions.assertThat(employe.getTempsPartiel()).isEqualTo(1);
+    }
 
+    @Test
+    public void testEmbaucheEmployeLimitMarticule() {
+        //Given
+        Mockito.when(employeRepository.findLastMatricule()).thenReturn("99999");
+
+        //When
+        Throwable t = Assertions.catchThrowable(() -> {
+                employeService.embaucheEmploye("Doe", "John", Poste.COMMERCIAL, NiveauEtude.MASTER, 1.0);
+        });
+        //Then
+        Assertions.assertThat(t).isInstanceOf(EmployeException.class).hasMessage("Limite des 100000 matricules atteinte !");
 
     }
 
+    @Test
+    public void testEmbaucheEmployeExist(){
+        //G
+        Mockito.when(employeRepository.findLastMatricule()).thenReturn(null);
+        Mockito.when(employeRepository.findByMatricule("C00001")).thenReturn(new Employe());
 
+        //When
+        Throwable t = Assertions.catchThrowable(() -> {
+            employeService.embaucheEmploye("Doe", "John", Poste.COMMERCIAL, NiveauEtude.MASTER, 1.0);
+        });
 
+        Assertions.assertThat(t).isInstanceOf(EntityExistsException.class).hasMessage("L'employé de matricule C00001 existe déjà en BDD");
 
+    }
 
 }
